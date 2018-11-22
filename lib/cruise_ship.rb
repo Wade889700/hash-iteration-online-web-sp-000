2fb30207711affdb9d67de4ea99cdff84caabd17
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 08:31:10 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,11 +6,13 @@
 # suite_e: "Crumpet the Elf"
 # }
 def select_winner(passengers)
-  passengers.each do |suite, name|
-    winner=""
-    if suite == "suite_a" && name.start_with?("A")
-      winner = name
-    end
+
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
+end
+
 winner
 end
