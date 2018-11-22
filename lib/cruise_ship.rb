e27ee0f899eebbdaf9f5681fc9ecaa10075ea4a6
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 08:28:01 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,8 +9,8 @@
 def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == "suite_a" && name.start_with("A")
-      Winner = name
+      winner = name
     end
   end
-  return Winner
+  return winner
 end
