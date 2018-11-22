a4b0431bda167c11dc9ff35805a997b0c8476569
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 08:22:39 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |word|
+  birthday_kids.each do |key, value|
     puts "Happy Birthday #{word}! You are now #{birthday_kids[word]} years old!"
   end
 end
